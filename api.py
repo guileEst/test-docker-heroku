e8b482228468ed4dc17fe81b23fbec046996aa75
ferchobeatmaker <101910450+ferchobeatmaker@users.noreply.github.com>
2022-03-19 18:11:05 -0500

    Update api.py

--- api.py
+++ api.py
@@ -8,3 +8,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_word():
     return "<B>Hello World!!</B>"
+@app.route("/buscar")
+def buscar_Producto_en_wallapop():
+    wallapopBaseUrl="https://api.wallapop.com/api/v3/general/"
+    wallapopoKeyWord="nintendo"
+#    wallapopoKeyWord=request.args.get('keywords', 'nintendo')
+    wallapopUrl=f"https://api.wallapop.com/api/v3/general/search?keywords={wallapopoKeyWord}%20&category_ids=12900&filters_source=seo_landing&longitude=-3.69196&latitude=40.41956&order_by=closest"
+    r = requests.get(wallapopUrl)
